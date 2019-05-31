d9ae1ff97c358ec0ce3ec14f4840ab3453a8bc7d
xff <572583146@qq.com>
2019-05-31 19:27:01 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	distance = inputs.distance;
+	parkTime = inputs.parkTime;
+	if (distance <= 2){
+		price = Math.round(6 + 0.25*parkTime);
+	} else if (distance <= 8){
+		price = Math.round(6 + 0.8*(distance-2) + 0.25*parkTime);
+	} else {
+		price = Math.round(6 + 0.8*6 + 1.2*(distance-8) + 0.25*parkTime);
+	}
+    return price;
 };
